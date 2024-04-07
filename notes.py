c40f74fd74803b08ce8050258acc38fe638fe31d
Evgeniy <Jack-inc@yandex.ru>
2024-04-07 15:09:46 +0400

    added class Notes

--- notes.py
+++ notes.py
@@ -1,12 +1,25 @@
+import datetime
+
+
+class Notes:
+    id = 0
+    notesList = {}
+
+    def __init__(self, txt):
+        Notes.id += 1
+        self.id = Notes.id
+        dt_now = datetime.datetime.now()
+        dt_string = dt_now.strftime('%d/%m/%y %H:%M:%S')
+        Notes.notesList[self] = [txt, dt_string]
 
 
 
 def main():
     while True:
-        print ('1 -  Добавить запись')
+        print('1 -  Добавить запись')
         print('0 - выйти из программы')
-        print('Введите номер пункта меню ', end ='')
-        menuItem= int(input ().strip())
+        print('Введите номер пункта меню ', end='')
+        menuItem = int(input().strip())
 
         match menuItem:
 
@@ -15,10 +28,12 @@ def main():
                 break
 
             case 1:
-                print(f"Команда  {menuItem}")
+                print("Введите текст заметки ", end='')
+                note = Notes(input())
+                print(f" Номер {note.id}")
+                print(Notes.notesList)
             case _:
                 print("Неверная команда")
 
-
 if __name__ == '__main__':
-        main()
+    main()
